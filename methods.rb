4fdbf051d5e5c6fcd9eecfc5162ced6eb8460bb7
Yontida Yosbut <a.yontida@gmail.com>
2021-12-18 01:04:28 -0500

    commit tree 8689ca2da07dcee33936a10fe881dba43baec464

--- methods.rb
+++ methods.rb
@@ -1,1 +1,40 @@
 # Your code here!
+#1 No argument
+def greet_programmer
+  puts "Hello, programmer!"
+end
+# greet_programmer
+
+
+#2 One argument
+def greet name
+  puts "Hello, #{name}!"
+end
+# puts greet "Naureen"
+
+
+#3 Default Arguments
+def greet_with_default(name = "programmer")
+  puts "Hello, #{name}!"
+end
+# greet_with_default
+# greet_with_default "Sunny"
+
+
+#4 Two arguments with return value
+def add(num1, num2)
+  num1 + num2
+end
+# sum = add(1, 2)
+
+
+#5 
+def halve(num)
+  if num.class != Integer
+  return nil
+
+  else
+  num/2
+  end
+  
+end
